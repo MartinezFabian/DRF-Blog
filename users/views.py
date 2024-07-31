2dc3949a8935a8ec90f1e8cab9346866af7ae8d5
MartinezFabian <fabianmartinezdev@gmail.com>
2024-07-31 09:02:53 -0300

    Add RegisterUser view and path

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+
+class RegisterUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            if user:
+                token = Token.objects.create(user=user)
+                json_user = serializer.data
+
+                return Response(
+                    {"token": token.key, "user": json_user},
+                    status=status.HTTP_201_CREATED,
+                )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
