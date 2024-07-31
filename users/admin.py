5255ab2939cae2854da39b0ebc60f2588231970d
MartinezFabian <fabianmartinezdev@gmail.com>
2024-07-31 17:25:28 -0300

    Add CustomUser administration panel customization in users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput, Textarea, CharField
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+# Personalizar el panel de admin para CustomUser
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ("email", "username", "first_name", "last_name")
+    list_filter = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
+    ordering = ("-date_joined",)
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+        ("Personal", {"fields": ("about",)}),
+    )
+    formfield_overrides = {
+        models.TextField: {"widget": Textarea(attrs={"rows": 20, "cols": 60})},
+    }
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
