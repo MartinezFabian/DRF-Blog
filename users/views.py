934684d349635b30557b2cafc33543dc2cd1e534
MartinezFabian <fabianmartinezdev@gmail.com>
2024-07-31 09:41:27 -0300

    Add LoginUser view and path

--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .serializers import CustomUserSerializer
+from users.models import CustomUser
 
 
 class RegisterUser(APIView):
@@ -25,3 +27,23 @@ class RegisterUser(APIView):
                 )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        user = get_object_or_404(CustomUser, email=request.data["email"])
+
+        if not user.check_password(request.data["password"]):
+            return Response(
+                {"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = CustomUserSerializer(instance=user)
+        json_user = serializer.data
+
+        return Response(
+            {"token": token.key, "user": json_user}, status=status.HTTP_200_OK
+        )
